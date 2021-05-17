b7f3ae8c006664a01f447005378ead4e34b6c7b7
Noufal Ibrahim <noufal@nibrahim.net.in>
2021-05-17 16:29:56 +0530

    Adds notes

--- 17-May/tables.c
+++ 17-May/tables.c
@@ -8,6 +8,14 @@ int main(void) {
   int b = get_int("Upto? ");
 
   for (int i=1; i<=b; i++) {
-    printf("%i x %2i = %2i\n", a, i, a*i);
+    printf("%2i x %2i = %2i\n", a, i, a*i);
+// %i will be substituted by a
+// x will be printed literally
+// %2i will be substituted by i but with a fixed field width
+// = will be printed literally
+// %2i will be substituted by a*i but with a fixed field width
+// \n will print a new line
+
+    
   }
 }
